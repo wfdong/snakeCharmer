c8734522e530c5e570d8176996df807717c71e1b
FuDong <sdjnwfd@126.com>
2018-11-15 21:56:25 +1100

    initial test file, without test code

---
+++ snakeCharmer/test/snakeCharmer/SnakeCharmerTest.java
@@ -0,0 +1,60 @@
+package snakeCharmer;
+
+public class SnakeCharmerTest {
+
+	public static void main(String[] args) {
+		Snake snake = new Snake();
+		SnakeCharmerTest test = new SnakeCharmerTest();
+		Zigzag zigzag = new Zigzag();
+		zigzag.setSnake(snake);
+	}
+	
+	/*
+	 * test for snake, LMove
+	 */
+	public void snakeTest_LMove(Snake snake) {
+		
+	}
+	
+	/*
+	 * test for snake, RMove
+	 */
+	public void snakeTest_RMove(Snake snake) {
+		
+	}
+	
+	/*
+	 * test for snake, getLMovePosition
+	 */
+	public void snakeTest_GetLMovePosition(Snake snake) {
+		
+	}
+	
+	/*
+	 * test for snake, getRMovePosition
+	 */
+	public void snakeTest_GetRMovePosition(Snake snake) {
+		
+	}
+	
+	/*
+	 * test for zigzag, getDestinationFromFile
+	 */
+	public void zigzagTest_GetDestinationFromFile(Zigzag zigag) {
+		
+	}
+	
+	/*
+	 * test for zigzag, writeResultToFile
+	 */
+	public void zigzagTest_writeResultToFile(Zigzag zigag) {
+		
+	}
+	
+	/*
+	 * test for zigzag, zigZag
+	 */
+	public void zigzagTest_zigzag(Zigzag zigag) {
+		
+	}
+}
